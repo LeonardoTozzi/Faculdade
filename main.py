06f28c695f04760d8811dbea6005468cff7b3dd7
LeonardoTozzi <leonardotozzis@gmail.com>
2025-03-24 15:17:33 -0300

    Apenas um script de calculo

---
+++ main.py
@@ -0,0 +1,12 @@
+valor1 = float(input("Digite o primeiro valor: "))
+print(valor1)
+valor2 = float(input("Digite o segundo valor: "))
+print(valor2)
+soma = valor1 + valor2
+print("A soma é {}".format(soma))
+subtracao = valor1 - valor2
+print("A subtração é {}".format(subtracao))
+divisao = valor1 / valor2 
+print("A divisão é {}".format(divisao))
+multiplicacao = valor1 * valor2 
+print("A multiplicação é {}".format(multiplicacao))
